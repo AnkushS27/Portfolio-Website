68d1a3137c8684306aea446e49f6dfaabbd430fd
AnkushS27 <ankush27singh11@gmail.com>
2023-06-08 10:05:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/portfolio", function(req, res){
+    res.render("portfolio");
+});
+app.get("/about", function(req, res){
+    res.render("about");
+});
+app.get("/contact", function(req, res){
+    res.render("contact");
+});
+
+// var day = "";
+
+// app.get("/contact", function(req, res){
+//     day = "xyz";
+//     res.render("abcd", {kindofday: day});
+// });
+
+app.listen(3000, function(){
+    console.log("Server is started at port 3000..");
+});
