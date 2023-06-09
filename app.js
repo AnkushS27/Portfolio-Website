532b67bd0d1d2d2477896f56feb75c06338bf683
AnkushS27 <ankush27singh11@gmail.com>
2023-06-09 19:04:13 +0530

    blog working

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
 
 // Routes for different pages
 app.get("/", function(req, res){
@@ -22,9 +24,59 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
     res.render("contact");
 });
+
+// Blog logic
+mongoose.connect("mongodb+srv://Ankush7163:ankushsingh490@cluster0.f50reqx.mongodb.net/myBlogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = {
+    title: String,
+    content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/blog", function(req, res){
-    res.render("blog");
+
+    Post.find({}, function(err, posts){
+      res.render("blog", {
+        posts: posts
+        });
+    });
 });
+
+app.get("/composeBlog", function(req, res){
+    res.render("compose_blog");
+});
+
+app.get("/blog/posts/:postId", function(req, res){
+
+    const requestedPostId = req.params.postId;
+    
+      Post.findOne({_id: requestedPostId}, function(err, post){
+        res.render("post", {
+          title: post.title,
+          content: post.content
+        });
+      });
+    
+});
+
+// Post requests
+app.post("/compose", function(req, res){
+    const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postBody
+      });
+    
+    
+      post.save(function(err){
+        if (!err){
+            res.redirect("/blog");
+        }
+      });
+})
+
+
 
 // var day = "";
 
